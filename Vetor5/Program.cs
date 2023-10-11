d99cc112b8baa72629218c008e684fe9d9e8b071
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-11 16:24:40 -0300

    Adicionar arquivos de projeto.

---
+++ Vetor5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+int N = int.Parse(Console.ReadLine());
+
+int[] nums =  new int[N];
+
+string[] vet = Console.ReadLine().Split(' ');
+
+for (int i = 0; i < N; i++) {
+    nums[i] = int.Parse(vet[i]);
+}
+
+double soma = 0.0;
+int qtd = 0;
+for (int i = 0; i < N; i++) {
+    if (nums[i] % 2 == 0) {
+        soma += nums[i];
+        qtd++;
+    }
+}
+
+if (qtd == 0) {
+    Console.WriteLine("Não há nenhum número par");
+} else {
+    double media = soma / qtd;
+    Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+}
